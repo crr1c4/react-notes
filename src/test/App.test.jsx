e8af56ee27f0e55ef92882c0932bd6ac4a6ea405
crr1c4 <german1c4@gmail.com>
2021-08-24 21:04:13 -0500

    Los test sin CreateReactApp son un dolor de cabeza :(

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "../App";
 
+test("Render App Content", () => {
+  const Application = render(<App />);
+
+  console.log(Application);
+});
