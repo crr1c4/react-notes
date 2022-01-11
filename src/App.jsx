f952fdffbb50a755f5d8dba0f722bcae60443cb5
crr1c4 <german1c4@gmail.com>
2022-01-11 10:48:59 -0600

    Refectoring code with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,52 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
-import MainScreen from './components/MainScreen'
-import SidePanel from './components/SidePanel'
+import Main from "./pages/Main";
 
-import { useTheme } from './hooks/useTheme'
-import { useFocus } from './hooks/useFocus'
-import { useLocalStorage } from './hooks/useLocalStorage'
+const AppRoutes = () => useRoutes([{ path: "/", element: <Main /> }]);
+
+// import MainScreen from './components/MainScreen'
+// import SidePanel from './components/SidePanel'
+
+// import { useTheme } from './hooks/useTheme'
+// import { useFocus } from './hooks/useFocus'
+// import { useLocalStorage } from './hooks/useLocalStorage'
 
 const App = () => {
-  const [theme, changeTheme] = useTheme()
-  const [focus, changeFocus] = useFocus()
-  const [storedNotes, setNotes, currentID, setCurrentID] = useLocalStorage('react-notes', [])
+  // const [theme, changeTheme] = useTheme()
+  // const [focus, changeFocus] = useFocus()
+  // const [storedNotes, setNotes, currentID, setCurrentID] = useLocalStorage('react-notes', [])
 
-  const styles = `sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-4 grid h-screen bg-white ${theme}`
+  // const styles = `sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-4 grid h-screen bg-white ${theme}`
 
-  const sidePanelProps = {
-    theme,
-    changeTheme,
-    focus,
-    changeFocus,
-    storedNotes,
-    setCurrentID
-  }
+  // const sidePanelProps = {
+  //   theme,
+  //   changeTheme,
+  //   focus,
+  //   changeFocus,
+  //   storedNotes,
+  //   setCurrentID
+  // }
 
-  const mainScreenProps = {
-    theme,
-    focus,
-    changeFocus,
-    setNotes,
-    storedNotes,
-    currentID,
-    setCurrentID
-  }
+  // const mainScreenProps = {
+  //   theme,
+  //   focus,
+  //   changeFocus,
+  //   setNotes,
+  //   storedNotes,
+  //   currentID,
+  //   setCurrentID
+  // }
 
   return (
-    <main className={styles}>
-      <SidePanel {...sidePanelProps} />
-      <MainScreen {...mainScreenProps} />
-    </main>
-  )
-}
+    // <main className={styles}>
+    //   <SidePanel {...sidePanelProps} />
+    //   <MainScreen {...mainScreenProps} />
+    // </main>
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+};
 
-export default App
+export default App;
